203668f6be11e99baf3146c85530d24543703cdd
unknown <s.adeboye@outlook.com>
2026-01-12 16:03:01 +0000

    chore: remove legacy mobile-app

--- mobile-app/src/utils/tier_utils.js
+++
@@ -1,5 +0,0 @@
-import { TIER_LIMITS } from './constants';
-
-export const isPremium = (tier) => tier === 'premium';
-
-export const getMaxScans = (tier) => (tier === 'premium' ? Infinity : TIER_LIMITS.free || 3);
