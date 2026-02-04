c714d2ddde5b55403d57cdb14eb39bda62014611
unknown <s.adeboye@outlook.com>
2026-02-04 19:19:30 +0000

    Add refresh token support

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -8,6 +8,7 @@ from .favorite import Favorite  # noqa: F401
 from .meal_plan import MealPlan  # noqa: F401
 from .shopping_item import ShoppingItem  # noqa: F401
 from .password_reset import PasswordResetToken  # noqa: F401
+from .refresh_token import RefreshToken  # noqa: F401
 from .admin_user import AdminUser  # noqa: F401
 from .recipe import Recipe  # noqa: F401
 from .recipe_history import RecipeHistory  # noqa: F401
